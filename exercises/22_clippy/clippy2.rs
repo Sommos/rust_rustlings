73147c1c73876d94ab782e3c4a805d7300faac97
Sommos <Sommosgaming@gmail.com>
2024-11-29 21:41:43 +0000

    ADD: 22_clippy

--- exercises/22_clippy/clippy2.rs
+++ exercises/22_clippy/clippy2.rs
@@ -2,7 +2,8 @@ fn main() {
     let mut res = 42;
     let option = Some(12);
     // TODO: Fix the Clippy lint.
-    for x in option {
+    // #[allow(for_loops_over_fallibles)]
+    if let Some(x) = option {
         res += x;
     }
 
